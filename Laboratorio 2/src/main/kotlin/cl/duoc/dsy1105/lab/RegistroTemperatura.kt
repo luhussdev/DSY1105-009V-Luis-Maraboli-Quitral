a113e5ada7bb08c9dd23e005cc389c576bbd2a0a
Luhuss <l.maraboliq@gmail.com>
2026-08-19 18:47:58 -0400

    feat(laboratorio 2): Se completa parte B puntos 3, 4 y 5 con filter, count y map

--- Laboratorio 2/src/main/kotlin/cl/duoc/dsy1105/lab/RegistroTemperatura.kt
+++ Laboratorio 2/src/main/kotlin/cl/duoc/dsy1105/lab/RegistroTemperatura.kt
@@ -9,6 +9,15 @@ fun main(){
         println("La temperatura $t°C es $clasificacion")
     }
 
+    // Devuelve una nueva lista con los elementos que cumplen la nueva condición asignada
+    val alta = temp.filter { it >= 25 }
+    println("Temperaturas >= 25°C: $alta")
+    println("Cantidad de temperaturas altas: ${alta.count()}")
+
+    // .map transforma los valores de la colección. En este caso transforma todos los valores de la lista a un incremento de + 1
+    val aumentoUnGrado = temp.map { it + 1 }
+    println("Temperaturas incrementadas en 1°C: $aumentoUnGrado")
+
 }
 
 fun clasificarTemperaturas(temp: Int): String{
